76fae2774ea5653f4b7d10789bfda9a1d51b29e5
lilliputten <lilliputten@gmail.com>
2025-04-26 19:51:04 +0300

    Build all4promo-calculator v.0.0.1 / 2025.04.24 17:35:27 +0300

---
+++ app-config-override.SAMPLE.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @descr Override mail configuration for the specific environment
+ * @changed 2025.04.26, 17:54
+ */
+
+// See all the overridable variables in the `app-config.php` script...
---
+++ app-config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @descr Application configuration
+ * @changed 2025.04.26, 17:53
+ */
+
+$dataFileName = 'data/data.json';
+$sampleUpdatePricesDataFileName = 'data/sample-update-data.json';
+
+/** Return error if sent from another address */
+$checkUrlPrefix = 'https://constructor.all4promo.ru/';
+
+// [>* These fields will be checked and script won't process in case of their absence <]
+// $requiredFields = array(
+//   'name',
+//   'email',
+//   'phone',
+// );
